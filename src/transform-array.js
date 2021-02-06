ba162ba133f3a098fad64cf88bdad41382be7522
Born <yura.yushkevich@gmail.com>
2021-02-06 13:28:35 +0300

    refactor: add function transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,171 +1,37 @@
 'use strict';
-// const CustomError = require("../extensions/custom-error");
+const CustomError = require("../extensions/custom-error");
 
-// module.exports = function transform(arr) {
-//   let itemPos =-1;
-//    if (Array.isArray(arr)){
-//       const arrDel= ['--double-next','--double-prev','--discard-prev','--discard-next'];
-//       const itemArray = arr.filter((item)=>{
-//           return !arrDel.includes(item);
-//       });
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)){
+    throw 'Error';
+  }
 
-//       for(let i=0; i<arr.length;i++){
-//         if(arr[i] == '--double-next'){
-//           itemPos = itemArray.indexOf(arr[i+1]);          
-//           let res = (itemPos != -1) ? itemArray.splice(itemPos,0,arr[i+1]) : false;            
-//         }
-//         else if(arr[i] == '--double-prev'){
-//           itemPos = itemArray.indexOf(arr[i-1]);          
-//           let res = (itemPos != -1) ? itemArray.splice(itemPos,0,arr[i-1]) : false;
-//          }
-//         else if(arr[i] == '--discard-prev'){
-//           itemPos = itemArray.indexOf(arr[i-1]);          
-//           let res = (itemPos != -1) ? itemArray.splice(itemPos,1) : false;
+    let itemArray = [...arr];
  
-//         }
-//         else if(arr[i] == '--discard-next'){
-//           itemPos = itemArray.indexOf(arr[i+1]);          
-//           let res = (itemPos != -1) ? itemArray.splice(itemPos,1) : false;
-//         }     
-//      }
-//      return itemArray; 
-//    }
-//    else{
-//      throw Error;
-//    }
-// };
-
-
-
-
-
-
-
-
-
-
-function transform(arr) {
-  let itemPos =[];
-   if (Array.isArray(arr)){
-      const arrDel= ['--double-next','--double-prev','--discard-prev','--discard-next'];
-      const itemArray = arr.filter((item)=>{
-          return !arrDel.includes(item);
-      });    
-       
-
-      for(let i=0; i<arr.length;i++){
-        if(arr[i] == '--double-next'){
-          // itemPos = itemArray.indexOf(arr[i+1]); 
-          const itemPos = getAllIndexes(itemArray, arr[i+1]);         
-          let res = (itemPos[0] != -1) ? itemArray.splice(itemPos[0],0,arr[i+1]) : false;
-          itemPos.splice(itemPos[0],1);            
-        }
-        else if(arr[i] == '--double-prev'){
-          // itemPos = itemArray.indexOf(arr[i-1]);
-          const itemPos = getAllIndexes(itemArray, arr[i-1]);           
-          let res = (itemPos[0] != -1) ? itemArray.splice(itemPos[0],0,arr[i-1]) : false;
-          itemPos.splice(itemPos[0],1); 
+       for(let i=0; i<arr.length;i++){
+         if(arr[i] == '--double-next'){
+           itemArray.splice(i,1,arr[i+1]);          
          }
-        else if(arr[i] == '--discard-prev'){
-          const itemPos = getAllIndexes(itemArray, arr[i-1]);
-          // itemPos = itemArray.indexOf(arr[i-1]);          
-          let res = (itemPos[0] != -1) ? itemArray.splice(itemPos[0],1) : false;
-          itemPos.splice(itemPos[0],1); 
-        }
-        else if(arr[i] == '--discard-next'){
-          const itemPos = getAllIndexes(itemArray, arr[i+1]);
-          // itemPos = itemArray.indexOf(arr[i+1]);          
-          let res = (itemPos[0] != -1) ? itemArray.splice(itemPos[0],1) : false;
-          itemPos.splice(itemPos[0],1);
-        }     
-     }
-     return itemArray; 
-   }
-   else{
-     throw Error;
-   }
-
-}
-
-function getAllIndexes(arr, val) {
-  let indexes = [], i;
-  for(i = 0; i < arr.length; i++){
-      if (arr[i] === val){
-          indexes.push(i);
+          else if(arr[i] == '--double-prev'){
+           itemArray.splice(i,1,itemArray[i-1]);
+          }
+          else if(arr[i] == '--discard-next'){    
+            itemArray.splice(i+1,1,null);
+          } 
+          else if(arr[i] == '--discard-prev'){
+            if(i-1<0){
+              itemArray.splice(0, 1, null);
+            }else{
+              itemArray.splice(i-1, 1, null); 
+            }
+          }
       }
-  }
-  if (indexes.length!=0){
-     return indexes;
-  }else{
-    indexes[0]=-1;
-     return indexes; 
-  }
-}
+ 
+      const arrDel= ['--double-next','--double-prev','--discard-prev','--discard-next', undefined, null];
+      itemArray = itemArray.filter((item)=>{
+          return !arrDel.includes(item);
+      }); 
+ 
+      return itemArray; 
 
-
-
-
-
-
- console.log(transform([ 
-  '--double-prev',
-  1,
-  '--discard-prev',
-  'GHI',
-  '--discard-next',
-  333,
-  '--discard-prev',
-  { '0': 'first', '1': 'second', length: 2 },
-  '--double-prev',
-  1,
-  '--double-prev',
-  'DEF',
-  { '0': 'first', '1': 'second', length: 2 },
-  { '0': 'first', '1': 'second', length: 2 },
-  1.233,
-  333,
-  'GHI',
-  '--discard-prev'
-  ]));
-
-
-
-// [ '-1': '--double-prev' ]
-// !!!!!
-// []
-
-
-// [
-//   '--double-prev',
-//   1,
-//   '--discard-prev',
-//   'GHI',
-//   '--discard-next',
-//   333,
-//   '--discard-prev',
-//   { '0': 'first', '1': 'second', length: 2 },
-//   '--double-prev',
-//   1,
-//   '--double-prev',
-//   'DEF',
-//   { '0': 'first', '1': 'second', length: 2 },
-//   { '0': 'first', '1': 'second', length: 2 },
-//   1.233,
-//   333,
-//   'GHI',
-//   '--discard-prev'
-// ]
-// !!!!!
-// [
-//   'GHI',
-//   { '0': 'first', '1': 'second', length: 2 },
-//   { '0': 'first', '1': 'second', length: 2 },
-//   1,
-//   1,
-//   'DEF',
-//   { '0': 'first', '1': 'second', length: 2 },
-//   { '0': 'first', '1': 'second', length: 2 },
-//   1.233,
-//   333
-// ]
-
+};
