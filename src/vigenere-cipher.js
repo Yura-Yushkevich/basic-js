9c0594f9277d4025ad9acf2ed092237263b0440e
Born <yura.yushkevich@gmail.com>
2021-02-08 21:10:26 +0300

    refactor: add full key function

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -11,20 +11,30 @@ class VigenereCipheringMachine {
     if(!message || !key){
       throw 'Error';
     }
+    this.getSquare();
+    let str = "";
 
     message = message.toUpperCase();
     key = key.toUpperCase();
 
+       
+    key = this.getFullKey(message, key);
+    console.log(key);
+
     if (message.length !== key.length){
       return false;
-    } 
-    this.getSquare();
-    let str = "";
+    }
+    
+    
+    
     for (let i = 0; i < message.length; i++) {
       if (this.arrEN.indexOf(message[i])== -1){
         str +=message[i];
       }
-      str += this.square[this.arrEN.indexOf(message[i])][this.arrEN.indexOf(key[i])];
+      else{
+        str += this.square[this.arrEN.indexOf(message[i])][this.arrEN.indexOf(key[i])];
+      }
+      
       console.log(str);      
     }
     return str;
@@ -38,15 +48,15 @@ class VigenereCipheringMachine {
 
     message = message.toUpperCase();
     key = key.toUpperCase();
-
-
-    if (message.length !== key.length) return false;
     this.getSquare();
     let str = "";
+
+    if (message.length !== key.length) return false;
+    
     for (let i = 0; i < message.length; i++) {
-        let row = this[arrEN].indexOf(key[i])
-            coll = this.square[row].indexOf(message[i]);
-        str += this[arrEN][coll];
+        let row = this.arrEN.indexOf(key[i]);
+        let coll = this.square[row].indexOf(message[i]);
+        str += this.arrEN[coll];
     }
     return str;
 
@@ -59,11 +69,41 @@ class VigenereCipheringMachine {
         this.square[i] = this.arrEN.slice(i).concat(this.arrEN.slice(0, i));        
     }
   }
+
+  getFullKey(message, key){
+    // let newKey = [];
+    // let tempMessage = message.split(' ');
+
+    
+    // for (let i=0; i < tempMessage.length; i++){
+    //   let raz = tempMessage[i].length - key.length;
+    //   if (raz < 0 ){
+    //     newKey.push(key.substr(0, key.length - (-raz)));
+    //   }
+    //   else if ((raz > 0 )){
+    //     let keyPlus = key;
+    //     for (let k=0; k < raz; k++){           
+    //       keyPlus +=keyPlus[k];
+    //       // console.log(keyPlus);
+    //     }
+    //     newKey.push(keyPlus);
+    //   }
+            
+    // }
+    // return newKey.join(' ');
+
+
+  let newKey = [];
+    for (let k=0; k<message.length;k++){
+      for
+    }
+
+
 }
 
 module.exports = VigenereCipheringMachine;
 
 
 const directMachine = new VigenereCipheringMachine();
-console.log(directMachine.encrypt('attack at dawn!', 'alphonsefdethkl'));
+console.log(directMachine.encrypt('attack at dawn!', 'alphonse'));
 // , 'AEIHQX SX DLLU!');
